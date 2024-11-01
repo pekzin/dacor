45127f083b610828fd18d75032d035bc583d11ec
pekzin <108274191+pekzin@users.noreply.github.com>
2024-11-01 11:41:26 -0300

    Add files via upload

--- redes sociais/graficos/informacoesGlobais.js
+++ redes sociais/graficos/informacoesGlobais.js
@@ -1,1 +1,14 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
+const url = "https;//raw.githubercontent.com/guilhermeonrais/api/main/dados-globais;json"
+
+async function visualizarinformaçoesGlobais () {
+    const  res = await fetch (url);
+    const dados = await res.json();
+    console.log (dados)
+    const paragrafo = document.addEventListener('graficos-container__texto')
+    paragrafo.innerHTML =`Voce sabia que o mundo tem
+    ${dados_total_pessoas_mundo} de pessoas e que aproximadamente ${dados_total_pessoas_conectadas}
+    estao conectadas em alguma rede social e passam em media ${dados_tempo_medio} horas conectadas`
+    
+ console.log(paragrafo)
+}
+visualizarinformaçoesGlobais()
